d90c4b473f6f91b34651575ed46ada68408f2ce7
gugugu625 <18210965650@163.com>
2024-04-23 23:01:44 +0800

    Change data sources to registry

--- HardwareMonitor625/MainWindow.xaml.cs
+++ HardwareMonitor625/MainWindow.xaml.cs
@@ -13,9 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
-using OpenHardwareMonitor.Hardware;
 using Newtonsoft.Json;
 using System.IO;
+using Microsoft.Win32;
 using System.Threading;
 
 namespace HardwareMonitor625
@@ -27,19 +27,10 @@ namespace HardwareMonitor625
     public partial class MainWindow : Window
     {
         System.Timers.Timer timer;
-        Computer myComputer;
         SerialPort DevicePort;
-        UpdateVisitor updateVisitor;
         public MainWindow()
         {
             InitializeComponent();
-
-            myComputer = new Computer();
-            updateVisitor = new UpdateVisitor();
-            myComputer.Open();
-            myComputer.CPUEnabled = true;
-            myComputer.GPUEnabled = true;
-            myComputer.RAMEnabled = true;
 
             Button exitbutton = new Button { Content = "退出" };
             exitbutton.Click += ExitButton_Click;
@@ -68,81 +59,28 @@ namespace HardwareMonitor625
             //this.notifyicon.Visibility = Visibility.Visible;
             //DevicePort.WriteLine("TEST");
         }
-        public HardwareDataStorage GetData(Computer myComputer, UpdateVisitor updateVisitor)
+
+        public float GetRegistry(string name)
         {
-            myComputer.Accept(updateVisitor);
-            HardwareDataStorage data = new HardwareDataStorage();
-            foreach (var hardwareItem in myComputer.Hardware)
+            RegistryKey key = Registry.CurrentUser;
+            RegistryKey software = key.OpenSubKey(@"Software\FinalWire\AIDA64\SensorValues", false);
+            string value = software.GetValue(name).ToString();
+            if(value == "")
             {
-                if (hardwareItem.HardwareType == HardwareType.CPU)
-                {
-                    foreach (var sensor in hardwareItem.Sensors)
-                    {
-                        //CPU温度
-                        if (sensor.SensorType == SensorType.Temperature)
-                        {
-                            if (sensor.Name == "CPU Package")
-                            {
-                                data.CPUTemperature = (float)sensor.Value;
-                                //Console.WriteLine(sensor.Name + " " + sensor.Value);
-                            }
-                        }
-                        //CPU占用
-                        if (sensor.SensorType == SensorType.Load)
-                        {
-                            if (sensor.Name == "CPU Total")
-                            {
-                                data.CPULoad = (float)sensor.Value;
-                            }
-                        }
-                    }
-                }
-                if (hardwareItem.HardwareType == HardwareType.GpuNvidia)
-                {
-                    foreach (var sensor in hardwareItem.Sensors)
-                    {
-                        //GPU温度
-                        if (sensor.SensorType == SensorType.Temperature)
-                        {
-                            if (sensor.Name == "GPU Core")
-                            {
-                                data.GPUTemperature = (float)sensor.Value;
-                            }
-
-                        }
-                        //GPU占用
-                        if (sensor.SensorType == SensorType.Load)
-                        {
-                            if (sensor.Name == "GPU Core")
-                            {
-                                data.GPULoad = (float)sensor.Value;
-                            }
-                            //GPU内存占用/空闲
-                            if (sensor.Name == "GPU Memory")
-                            {
-                                data.GPUMemoryUsed = (float)sensor.Value;
-                            }
-                        }
-                    }
-                }
-                if (hardwareItem.HardwareType == HardwareType.RAM)
-                {
-                    foreach (var sensor in hardwareItem.Sensors)
-                    {
-                        //内存占用/空闲
-                        if (sensor.SensorType == SensorType.Load)
-                        {
-                            if (sensor.Name == "Memory")
-                            {
-                                data.RAMUsed = (float)sensor.Value;
-                            }
-                        }
-                    }
-                }
-
-
-
+                return 0;
             }
+            return float.Parse(value);
+        }
+        public HardwareDataStorage GetData()
+        {
+
+            HardwareDataStorage data = new HardwareDataStorage();
+            data.CPUTemperature = GetRegistry("Value.TCPU");
+            data.CPULoad = GetRegistry("Value.SCPUUTI");
+            data.GPUTemperature = GetRegistry("Value.TGPU1");
+            data.GPULoad = GetRegistry("Value.SGPU1UTI");
+            data.GPUMemoryUsed = GetRegistry("Value.SVMEMUSAGE");
+            data.RAMUsed = GetRegistry("Value.SMEMUTI");
             return data;
         }
         private void ExitButton_Click(object sender, RoutedEventArgs e)
@@ -158,7 +96,7 @@ namespace HardwareMonitor625
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            HardwareDataStorage storage = GetData(myComputer, updateVisitor);
+            HardwareDataStorage storage = GetData();
             CPUTemperature.Content = "CPU温度"+storage.CPUTemperature;
             CPULoad.Content = "CPU占用" + storage.CPULoad;
             GPULoad.Content = "GPU占用" + storage.GPULoad;
@@ -169,7 +107,7 @@ namespace HardwareMonitor625
 
         private void SendData(object sender, System.Timers.ElapsedEventArgs e)
         {
-            HardwareDataStorage storage = GetData(myComputer,updateVisitor);
+            HardwareDataStorage storage = GetData();
             /*if (!Dispatcher.CheckAccess())
             {
                 Dispatcher.Invoke(() => SendData(sender,e));
